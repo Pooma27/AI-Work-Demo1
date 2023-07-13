8682b93a8cadade24b33e638612ec913bd9f92a8
Pooma27 <pooma27sripoom@gmail.com>
2023-07-13 17:11:24 +0700

    Create if_else.py
    IF else class

---
+++ if_else.py
@@ -0,0 +1,84 @@
+'''
+money = 100
+
+if money >= 300:
+    print("KFC!!!")
+else:
+    print("Byebye")
+'''
+'''
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+
+
+if username == "12345" and password == "BCC":
+    print("Poom Password") 
+elif username == "Cheese" and password == "Assumption":
+    print("Hacker Password")
+else: 
+    print("Your Username or Password is incorrect")
+'''
+'''
+user = input("Enter your username: ")
+passw = input("Enter your password: ")
+
+if user == "admin" and passw == "admin":
+    print("ok")
+    print("1 = VAT Cal")
+    print("2 = cal")
+    num = input("Select Number>>")
+    if num == "1":
+        price = int(input("Enter your price"))
+        vat = 7/100
+        print(price + (price * vat))
+    elif num =="2":
+        x = int(input("Enter number"))
+        y = int(input("Enter number"))
+        print(x + y)
+    else:
+        print("false")
+
+'''
+'''
+x = 0
+while x < 5:
+    x = x+1
+    print (x)
+'''
+'''
+user1 = input("Enter username: ")
+pass1 = input("Enter password: ")
+'''
+
+# if user == "Hello" and pass1 == "69":
+#     print("Valid gmail account")
+
+# else:
+#  print("Please retry your username/password")
+ 
+'''
+while user1 != "Hello" and pass1 != "69": 
+
+    user1 = input("Enter username: ")
+    pass1 = input("Enter password: ")
+'''
+
+'''
+user1 = input("Enter username: ")
+pass1 = input("Enter password: ") 
+
+x = ("incorrect")
+
+if user1 == "Hi" and pass1 == "11":
+    print("Correct")
+
+else:
+    print(x)
+while user1 != "Hi" and pass1 != "11":
+    
+    user1 = input("Enter username: ")
+    pass1 = input("Enter password: ")   
+'''
+
+while x == 0
+    user = input("Enter Your Name ")
