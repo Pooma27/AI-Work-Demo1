5a0b42980e54660a10111b3a498ec82e57cc9511
Pooma27 <pooma27sripoom@gmail.com>
2023-07-13 15:21:48 +0700

    add index and vatcal

---
+++ VAT_Cal.py
@@ -0,0 +1,11 @@
+'''
+price = int(input("Enter the price: "))
+
+vat = 7/100 * price
+
+print("The vat cost from the price is" , vat)
+
+total = vat + price
+
+print("The total price including vat is", total)
+'''
---
+++ index.py
@@ -0,0 +1,63 @@
+'''
+print("hello")
+print("123123")
+print(1)
+print(1.2)
+print(7j + 8j)
+print(True)
+print(False)
+'''
+'''Data Types'''
+#print(type("hello"))
+#print(type(1))
+#print(type(1.22))
+#print(type(True))
+
+
+'''Test'''
+#print(1+1)
+#print(1.2+1)
+#print("hello" + "Poom")
+
+'''Operator
++,-*,/,**'''
+
+'''
+print(1==1)
+print(2**4)
+
+x = 6
+y = "Hello"
+
+print(x)
+
+x = int(input())
+print(x)
+'''
+
+'''Calculator'''
+'''
+x = int(input("first number: "))
+y = int(input("second number: "))
+z = x+y
+print(x, "+", y, "=" , z )
+
+
+x = int(input("first number: "))
+y = int(input("second number: "))
+z = x*y
+print(x, "*", y, "=" , z )
+'''
+
+#a = int(input("First value: "))
+#b = int(input("Second value: "))
+#c = a/b
+#print("The result is", a, "/", b, "=" , c)
+
+#input name lastname
+
+name = input("Enter your Name: ")
+last = input("ENter your lastname: ")
+
+print(name, last)
+print(name + last)
